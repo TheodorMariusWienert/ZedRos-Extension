ac0aea53aaf14818f066a32c80822c1f948d8ca1
TheodorMariusWienert <theo@wienert-hamburg.de>
2022-10-12 23:23:24 +0200

    Updated ReadMe

--- README.md
+++ README.md
@@ -1,23 +1,52 @@
+This work is an extension of the [zed-ros-wrapper](https://github.com/stereolabs/zed-ros-wrapper).
+It is now possible to use a custom [YOLOv5](https://github.com/ultralytics/yolov5) Model to detect all classes the YOLOv5 model was trained on.
+It archieves that by using the [YOLOv5-TensorRT](https://github.com/noahmr/yolov5-tensorrt), many thanks to the author.
 
-Cuda 11.7
-https://developer.nvidia.com/tensorrt
-Tensorrt  8.4.2-1
-https://developer.nvidia.com/tensorrt
+How the extension was implemented is based on [ZED-YOLOv5]( https://github.com/noahmr/zed-yolov5) and [Yolov5 with ZED Custom Box input](https://github.com/stereolabs/zed-examples/tree/master/object%20detection/custom%20detector/cpp/tensorrt_yolov5_v6.0=
 
-https://developer.nvidia.com/cuda-11-7-1-download-archive?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=20.04&target_type=deb_local
 
-OpenCV ( with Cuda, this installation guide was used to install)
-yolov5-tensorrt (which need  cuda >= 10.2 and TensorRT >=8)
 
-@misc{yolov5tensorrt,
-	author       = {van der Meer, Noah and van Hoof, Charel},
-	title        = {{yolov5-tensorrt}: Real-time object detection with {YOLOv5} and {TensorRT}},
-	howpublished = {GitHub},
-	year         = {2021},
-	note         = {\url{https://github.com/noahmr/yolov5-tensorrt}}
-}
-ZED SDK 3.7 version
-https://www.stereolabs.com/developers/release/
-Ros Noetic
-http://wiki.ros.org/noetic/Installation/Ubuntu
-the-zed-ros-wrapper and the examples installedwith this guide
+To install this package you need first to install the following libaries:
+##Libraries needed
+- Cuda >10.02
+- Tensorrt  > 8
+- OpenCV (when possible wiht CUDA)
+- yolov5-tensorrt
+- ZED SDK
+- Ros Noetic
+## It was tested with these libaries instaled
+- Cuda 11.7
+- Tensorrt  8.4.2-1
+- OpenCV with Cuda
+- yolov5-tensorrt
+- ZED SDK 3.7 version
+- Ros Noetic
+
+And a ZED2 was used. Further a YOLOv5s model was use as a FP16 and FP32 TensorRT Engine.
+
+To install this project you need to do the following (analog to [instructions](https://www.stereolabs.com/docs/ros/) given by stereolabs):
+
+$cd ~/catkin_ws/src
+$ git clone --recursive https://github.com/TheodorMariusWienert/ZedRos-Extension
+$ cd ../
+$ rosdep install --from-paths src --ignore-src -r -y    # This gives some errors sometime but can be ignored if they dont affect the code
+$ catkin_make -DCMAKE_BUILD_TYPE=Release
+$ source ./devel/setup.bash
+
+
+Now you need to modify the zed2.yaml and place the path to your TensorRT engine in the file. And specify to use a custom detector. Also you can enter the names of the classes you want to detect here.
+
+To use your custom Yolov5 model you need to either export it to a FP32 engine with the export functionality of [YOLOv5](https://github.com/ultralytics/yolov5). Or when you want to use a FP16 engine you need to first export it to Onnx format with  [YOLOv5](https://github.com/ultralytics/yolov5) and than use the [YOLOv5-TensorRT](https://github.com/noahmr/yolov5-tensorrt) functionality to export it to a FP16 TensorRT engine.
+
+
+To start the programm  use:
+$ roslaunch zed_wrapper zed2.launch
+For visulaization use:
+$ roslaunch zed_display_rviz display_zed2.launch
+
+##For Time recording
+To record times you need to eddit zed_wrapper_nodelet.hpp and edit the pathFile and also set time recording to true
+To record times at subscriber site you need to edit the  zed_obj_det_sub and change the bools to true also set the path according to your paths
+To process the objects a numerous of Jupyter notebooks are in the Jupyter notebooks folder
+
+To use GPU MATS use the branch GPU Mat, CV with CUDA needs to be installed, this was not tested really much use with care
---
+++ src/zed-ros-examples/tutorials/zed_obj_det_sub_tutorial/src/zed_obj_det_sub_tutorial.cpp
@@ -0,0 +1,238 @@
+///////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2018, STEREOLABS.
+//
+// All rights reserved.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+///////////////////////////////////////////////////////////////////////////
+
+/**
+ * This tutorial demonstrates how to receive the list of detected objects from a ZED node
+ * from the ZED node
+ */
+
+#include <ros/ros.h>
+
+#include <zed_interfaces/Object.h>
+#include <zed_interfaces/ObjectsStamped.h>
+#include <iostream>
+#include <fstream>
+/**
+ * Subscriber callbacks. The argument of the callback is a constant pointer to the received message
+ */
+double timeForDetection[2][1000];//Time from image capture 
+double posData[5][300];
+int idx_toStart=0;
+int idx=0;
+bool timeRecording=true;
+bool posDataRecording=false;
+ros::Time lastStamp;
+
+
+void savePositionalData(){
+
+    std::ofstream posDataFile;
+     posDataFile.open ("/home/theubuntu/ZedRos_CustomDetection/YoloV5 ResultsonTest/posData_Tiago_1_6m.txt");
+        
+        for(int i = 0; i < (sizeof(posData[0])/sizeof(double)); ++i){
+            for(int j =0;j<(sizeof(posData)/sizeof(posData[0]));j++)
+            {
+              if(j==0)
+              {
+                posDataFile<<"class: "<<std::to_string(posData[j][i])+",";
+              }
+              else{
+                posDataFile<<std::to_string(posData[j][i])+",";
+              }
+                
+               
+            }
+              posDataFile<<"\n"; 
+        }
+       
+        posDataFile.close();
+                                          
+}
+void saveData(){
+
+    std::ofstream fileTimeDetect;
+     fileTimeDetect.open ("/home/theubuntu/ZedRos_CustomDetection/YoloV5 ResultsonTest/Retrieve+Ingest+Message+Publish/timesForDetectionSub.txt");
+        
+        for(int i = 0; i < (sizeof(timeForDetection[0])/sizeof(double)); ++i){
+            for(int j =0;j<(sizeof(timeForDetection)/sizeof(timeForDetection[0]));j++)
+            {
+                fileTimeDetect<<std::to_string(timeForDetection[j][i])+",";
+               
+            }
+              fileTimeDetect<<"\n"; 
+        }
+       
+        fileTimeDetect.close();
+                                          
+}
+
+void objectListCallback(const zed_interfaces::ObjectsStamped::ConstPtr& msg)
+{
+  //ROS_INFO("***** New object list *****");
+  for (int i = 0; i < msg->objects.size(); i++)
+  {
+    if (msg->objects[i].label_id == -1)
+      continue;
+     
+   
+    
+
+    // ROS_INFO_STREAM(msg->objects[i].sublabel << " [" << msg->objects[i].label_id << "] - Pos. ["
+    //                                       << msg->objects[i].position[0] << "," << msg->objects[i].position[1] << ","
+    //                                       << msg->objects[i].position[2] << "] [m]"
+    //                                       << "- Conf. " << msg->objects[i].confidence
+    //                                      // << " - Tracking state: " << static_cast<int>(msg->objects[i].tracking_state)
+    //                                       << " - Action state: " << static_cast<int>(msg->objects[i].action_state)
+    //                                       << msg->objects[i].bounding_box_3d.corners[4]
+    //                                       <<"\n"
+    //                                       << "widthBB"<<widthBB<<", "
+    //                                       << "heightBB"<<heightBB<<", "
+    //                                       << "deepthe"<<depthBB
+    //                                       <<"\n"
+    //                                       <<"min Distance X: "<<min_distanceX<<", "
+    //                                       <<"\n");
+    if(timeRecording||posDataRecording){
+    if(idx_toStart<100){
+      idx_toStart++;
+    }
+    if(timeRecording){
+       if(idx_toStart>=100 && idx<(sizeof(timeForDetection[0])/sizeof(double))){
+       
+        ros::Time stamp = msg->header.stamp; // Timestamp
+        
+        if(stamp!=lastStamp){
+        ros::Time nowStamp = ros::Time::now();//each image only once
+        double stamp_ms=(double(stamp.sec) + double(stamp.nsec)*1e-9)*1e3;
+        double nowStamp_ms=(double(nowStamp.sec) + double(nowStamp.nsec)*1e-9)*1e3;
+        double diff=nowStamp_ms-stamp_ms;
+        timeForDetection[0][idx]=diff;
+        timeForDetection[1][idx]=nowStamp_ms;
+        idx++;
+        lastStamp=stamp;
+        }
+        else{
+        //same img timestamp, same image differnt object"
+        double stamp_ms=(double(stamp.sec) + double(stamp.nsec)*1e-9)*1e3;
+        ros::Time nowStamp2 = ros::Time::now();
+        double nowStamp2_ms=(double(nowStamp2.sec) + double(nowStamp2.nsec)*1e-9)*1e3;
+        double diff=nowStamp2_ms-stamp_ms;
+        timeForDetection[0][idx-1]=diff;
+        timeForDetection[1][idx-1]=nowStamp2_ms;
+        }
+
+      }
+      if(idx==(sizeof(timeForDetection[0])/sizeof(double))){
+      saveData();
+        idx++;
+      }
+    }
+    else{
+       if(idx_toStart>=100 && idx<(sizeof(posData[0])/sizeof(double))){
+        
+         auto widthBB =msg->objects[i].bounding_box_3d.corners[4].kp[1]-msg->objects[i].bounding_box_3d.corners[7].kp[1];
+         auto heightBB = -msg->objects[i].bounding_box_3d.corners[4].kp[2]+ msg->objects[i].bounding_box_3d.corners[0].kp[2];
+        auto depthBB = -msg->objects[i].bounding_box_3d.corners[4].kp[0]+ msg->objects[i].bounding_box_3d.corners[5].kp[0];
+
+        auto min_distanceX= fmin(msg->objects[i].bounding_box_3d.corners[4].kp[0],msg->objects[i].bounding_box_3d.corners[7].kp[0]);
+
+   
+       
+
+        if( msg->objects[i].sublabel== "kuka") posData[0][idx]=0;
+        if( msg->objects[i].sublabel== "human") posData[0][idx]=1;
+        if( msg->objects[i].sublabel== "tiago") posData[0][idx]=2;
+        if( msg->objects[i].sublabel== "pepper") posData[0][idx]=3;
+        posData[1][idx]=widthBB;
+        posData[2][idx]=heightBB;
+        posData[3][idx]=depthBB;
+        posData[4][idx]=min_distanceX;;
+        idx++;
+   
+   // std::cout<<diff<<"\n";
+      }
+      if(idx==(sizeof(posData[0])/sizeof(double))){
+       savePositionalData();
+      idx++;
+      }
+
+
+
+    }
+   
+    }
+   
+  
+
+
+   
+  }
+}
+
+
+/**
+ * Node main function
+ */
+int main(int argc, char** argv)
+{
+  /**
+   * The ros::init() function needs to see argc and argv so that it can perform
+   * any ROS arguments and name remapping that were provided at the command line.
+   * For programmatic remappings you can use a different version of init() which takes
+   * remappings directly, but for most command-line programs, passing argc and argv is
+   * the easiest way to do it.  The third argument to init() is the name of the node.
+   *
+   * You must call one of the versions of ros::init() before using any other
+   * part of the ROS system.
+   */
+  ros::init(argc, argv, "zed_obj_det_sub_tutorial");
+
+  /**
+   * NodeHandle is the main access point to communications with the ROS system.
+   * The first NodeHandle constructed will fully initialize this node, and the last
+   * NodeHandle destructed will close down the node.
+   */
+  ros::NodeHandle n;
+
+  /**
+   * The subscribe() call is how you tell ROS that you want to receive messages
+   * on a given topic.  This invokes a call to the ROS
+   * master node, which keeps a registry of who is publishing and who
+   * is subscribing.  Messages are passed to a callback function, here
+   * called imageCallback.  subscribe() returns a Subscriber object that you
+   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
+   * object go out of scope, this callback will automatically be unsubscribed from
+   * this topic.
+   *
+   * The second parameter to the subscribe() function is the size of the message
+   * queue.  If messages are arriving faster than they are being processed, this
+   * is the number of messages that will be buffered up before beginning to throw
+   * away the oldest ones.
+   */
+  ros::Subscriber subObjList = n.subscribe("objects", 100, objectListCallback);
+
+  /**
+   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
+   * callbacks will be called from within this thread (the main one).  ros::spin()
+   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
+   */
+  ros::spin();
+
+  return 0;
+}
